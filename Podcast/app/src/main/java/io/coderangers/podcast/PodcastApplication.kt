8b856861dcc4bb7431525e6e4cd6818ded870474
davidchhetri <david_chhetri@yahoo.com>
2023-11-05 14:26:14 -0500

    initial commit

---
+++ Podcast/app/src/main/java/io/coderangers/podcast/PodcastApplication.kt
@@ -0,0 +1,9 @@
+package io.coderangers.podcast
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PodCastApplication: Application() {
+
+}
---
+++ Podcast/app/src/main/java/io/coderangers/podcast/model/api/response/Extra.kt
@@ -0,0 +1,33 @@
+package io.coderangers.podcast.model.api.response
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Extra(
+    @SerializedName("amazon_music_url")
+    val amazonMusicUrl: String,
+    @SerializedName("facebook_handle")
+    val facebookHandle: String,
+    @SerializedName("google_url")
+    val googleUrl: String,
+    @SerializedName("instagram_handle")
+    val instagramHandle: String,
+    @SerializedName("linkedin_url")
+    val linkedinUrl: String,
+    @SerializedName("patreon_handle")
+    val patreonHandle: String,
+    @SerializedName("spotify_url")
+    val spotifyUrl: String,
+    @SerializedName("twitter_handle")
+    val twitterHandle: String,
+    @SerializedName("url1")
+    val url1: String,
+    @SerializedName("url2")
+    val url2: String,
+    @SerializedName("url3")
+    val url3: String,
+    @SerializedName("wechat_handle")
+    val wechatHandle: String,
+    @SerializedName("youtube_url")
+    val youtubeUrl: String
+)
